ae8725de5f70246de1f779fab56719a57e2f0dc5
WizMager <wizmager@gmailcom>
2025-10-31 19:34:11 +0300

    -complete withh apply damage

---
+++ Assets/Scripts/Systems/ApplyDamageSystem.cs
@@ -0,0 +1,49 @@
+﻿using Components;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
+
+namespace Systems
+{
+    [UpdateInGroup(typeof(PredictedSimulationSystemGroup), OrderLast = true)]
+    [UpdateAfter(typeof(CalculateFrameDamageSystem))]
+    public partial struct ApplyDamageSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<NetworkTime>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var currentTick = SystemAPI.GetSingleton<NetworkTime>().ServerTick;
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+            foreach (var (currentHitPoints, damageThisTicks, entity) in SystemAPI
+                .Query<RefRW<CurrentHitPointsComponent>, DynamicBuffer<DamageThisTick>>()
+                .WithAll<Simulate>()
+                .WithEntityAccess()
+            )
+            {
+                if (!damageThisTicks.GetDataAtTick(currentTick, out var damageThisTick))
+                    continue;
+
+                if (damageThisTick.Tick != currentTick)
+                    continue;
+
+                currentHitPoints.ValueRW.Value -= damageThisTick.Value;
+
+                if (currentHitPoints.ValueRO.Value <= 0)
+                {
+                    ecb.AddComponent<DestroyEntityTag>(entity);
+                }
+            }
+            
+            ecb.Playback(state.EntityManager);
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/CalculateFrameDamageSystem.cs
@@ -0,0 +1,58 @@
+﻿using Components;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace Systems
+{
+    [UpdateInGroup(typeof(PredictedSimulationSystemGroup), OrderLast = true)]
+    public partial struct CalculateFrameDamageSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<NetworkTime>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var currentTick = SystemAPI.GetSingleton<NetworkTime>().ServerTick;
+
+            foreach (var (damageBuffer, damageThisTicks) in SystemAPI
+                .Query<DynamicBuffer<DamageBufferElement>, DynamicBuffer<DamageThisTick>>()
+                .WithAll<Simulate>())
+            {
+                if (damageBuffer.IsEmpty)
+                {
+                    damageThisTicks.AddCommandData(new DamageThisTick
+                    {
+                        Tick = currentTick,
+                        Value = 0
+                    });
+                }
+                else
+                {
+                    var totalDamage = 0;
+                    if (damageThisTicks.GetDataAtTick(currentTick, out var damageThisTick))
+                    {
+                        totalDamage = damageThisTick.Value;
+                    }
+
+                    foreach (var damageBufferElement in damageBuffer)
+                    {
+                        totalDamage += damageBufferElement.Value;
+                    }
+                    
+                    damageThisTicks.AddCommandData(new DamageThisTick
+                    {
+                        Tick = currentTick,
+                        Value = totalDamage
+                    });
+                    
+                    damageBuffer.Clear();
+                }
+            }
+        }
+    }
+}
